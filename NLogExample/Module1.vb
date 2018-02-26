d05ad27b1aaf6af4ea686ae24893e51e7bf869ab
lutz.brandt <lutz.brandt@doctari.de>
2018-02-26 10:57:48 +0100

    console logging

--- NLogExample/Module1.vb
+++ NLogExample/Module1.vb
@@ -1,6 +1,11 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim w = New Worker
+
+        w.Method1()
+
+        Console.ReadKey()
 
     End Sub
 
---
+++ NLogExample/Worker.vb
@@ -0,0 +1,20 @@
+﻿
+Imports System.Threading
+Imports NLog
+
+Public Class Worker
+
+    Private Shared Log As ILogger
+
+    Shared Sub New()
+        Log = LogManager.GetCurrentClassLogger()
+    End Sub
+
+    Public Sub Method1()
+        Log.Debug("StartMethod1")
+        Log.Info("Do some Work")
+        Thread.Sleep(100)
+        Log.Debug("EndMethod1")
+    End Sub
+
+End Class
